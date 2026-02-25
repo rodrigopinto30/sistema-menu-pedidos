04e98eb91a492208823d415d5435b26a35e758ff
Rodrigo <rodrigo.pinto@kyas.com.ar>
2026-02-25 08:25:31 -0300

    feat: implement store method in OrderController with database transactions

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MenuController;
+use App\Http\Controllers\Api\OrderController;
 
 Route::get('/menu', [MenuController::class, 'index']);
+
+Route::post('/orders', [OrderController::class, 'store']);
